5d63d06625192f1124430dfe05eb9e4f30e4a790
bradwco <bradco626@gmail.com>
2023-01-19 16:55:19 -0800

    almostdone

--- main.js
+++ main.js
@@ -5,3 +5,27 @@ function Random() {
     alert(rnd);
 }
 
+function changeImg(){
+    var image = "";
+    if(rnd==1){
+        image = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Dice-1-b.svg/1024px-Dice-1-b.svg.png"
+    }
+    else if(rnd==2){
+        image =""
+    }
+    else if(rnd==3){
+        image =""
+    }
+    else if(rnd==4){
+        image =""
+    }
+    else if(rnd==5){
+        image =""
+    }
+    else if(rnd==6){
+        image =""
+    }
+    else{
+        alert('ERROR')
+    }
+}
